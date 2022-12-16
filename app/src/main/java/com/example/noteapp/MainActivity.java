a3600decc9b86e8df3292af7fe355121ff35fa70
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-15 20:04:02 -0800

    Initialized Note Details Activity

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.action_btn);
+        floatingActionButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, NoteDetailsActivity.class)));
     }
 }
