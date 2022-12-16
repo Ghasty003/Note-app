a7a6d80ceac4c2346e20bd77a64e25a7b440610f
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-15 20:26:26 -0800

    Completed Note Details Activity

--- app/src/main/java/com/example/noteapp/NoteDetailsActivity.java
+++ app/src/main/java/com/example/noteapp/NoteDetailsActivity.java
@@ -3,12 +3,33 @@ package com.example.noteapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class NoteDetailsActivity extends AppCompatActivity {
+
+    private EditText titleEditText, textEditText;
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_details);
+
+        titleEditText = findViewById(R.id.title);
+        textEditText = findViewById(R.id.text);
+        imageButton = findViewById(R.id.save_btn);
+
+        imageButton.setOnClickListener(v -> saveNote());
+    }
+
+    public void saveNote() {
+        String title = titleEditText.getText().toString();
+        String text = textEditText.getText().toString();
+
+        if (title == null || title.isEmpty()) {
+            titleEditText.setError("Title is required");
+            return;
+        }
     }
 }
